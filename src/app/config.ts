49aeed394beef93859995a795422b23dfb3d9e8b
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2024-12-01 04:50:01 +0200

    api ip fix

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    apiBaseUrl: 'http://localhost:5000',
+    apiBaseUrl: 'http://127.0.0.1:5000',
     //apiBaseUrl: 'http://51.84.9.120:5000',
     defaultLanguage: 'he',
     otherGlobalVariable: 'value',
