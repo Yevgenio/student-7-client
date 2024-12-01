69745268d65e7a56369be10b9eb5ae416c7ae76e
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2024-12-01 04:58:45 +0200

    public ip api

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
-    apiBaseUrl: 'http://127.0.0.1',
-    //apiBaseUrl: 'http://51.84.9.120:5000',
+    // apiBaseUrl: 'http://127.0.0.1',
+    apiBaseUrl: 'http://51.84.9.120',
     defaultLanguage: 'he',
     otherGlobalVariable: 'value',
   };
