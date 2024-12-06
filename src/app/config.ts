5c649db4af4754b9be6196a6da6ca9d3c3b49175
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2024-12-06 19:21:19 +0200

    changed backend address

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,7 @@
 export const CONFIG = {
-    // apiBaseUrl: 'http://127.0.0.1',
-    apiBaseUrl: 'http://51.84.9.120',
+    // apiBaseUrl: 'http://127.0.0.1', //localhost
+    // apiBaseUrl: 'http://51.84.9.120', // my server
+    apiBaseUrl: 'http://51.16.37.75', // student 7 aws
     defaultLanguage: 'he',
     otherGlobalVariable: 'value',
   };
