fb1648d2159675d5f598d07116f5496798d0a823
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2024-12-07 18:20:49 +0200

    domain change

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,8 @@
 export const CONFIG = {
     // apiBaseUrl: 'http://127.0.0.1', //localhost
     // apiBaseUrl: 'http://51.84.9.120', // my server
-    apiBaseUrl: 'http://51.16.37.75', // student 7 aws
+    // apiBaseUrl: 'http://51.16.37.75', // student 7 aws
+    apiBaseUrl: 'student-7.boukingolts.art',
     defaultLanguage: 'he',
     otherGlobalVariable: 'value',
   };
