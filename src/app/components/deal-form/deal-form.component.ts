33665f58db687045aae5d1db208fb6b0c12d1f67
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-01-24 05:30:30 +0200

    deal.stock || -1;

--- src/app/components/deal-form/deal-form.component.ts
+++ src/app/components/deal-form/deal-form.component.ts
@@ -42,6 +42,7 @@ export class DealFormComponent implements OnInit {
       this.isEditMode = true;
       // Fetch the deal details and populate the form
       this.dealService.getDealById(this.dealId).subscribe(deal => {
+        deal.stock = deal.stock || -1;
         this.dealForm.patchValue(deal);
       });
     }
